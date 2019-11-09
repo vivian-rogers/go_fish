6b1a55c673da6ac2a67c235ceaa25f56b1e2a9b1
will-rogers <rogers@LAPTOP-5OQ4DUTC.localdomain>
2019-11-09 03:11:18 -0600

    created files

---
+++ card.cpp
@@ -0,0 +1,49 @@
+// FILE: card_demo.cpp
+// This is a small demonstration program showing how the Card and Deck classes are used.
+#include <iostream>    // Provides cout and cin
+#include <cstdlib>     // Provides EXIT_SUCCESS
+#include "card.h"
+#include "player.h"
+#include "deck.h"
+
+using namespace std;
+
+
+// PROTOTYPES for functions used by this demonstration program:
+
+Card::Card() {
+	
+}
+
+Card::Card(int rank, Suit s) {
+	
+}
+
+string Card::toString(){
+	
+}
+
+bool Card::sameSuitAs(const Card& c) {
+	
+}
+
+int Card::getRank() {
+	
+}
+
+string Card::suitString(Suit s) const {
+
+}
+
+string Card::rankString(int r) {
+	
+}
+
+bool operator ==(const Card& rhs) const {
+	
+}
+
+bool operator ==(const Card& rhs) const {
+	
+}
+
---
+++ player.cpp
@@ -0,0 +1,30 @@
+// FILE: card_demo.cpp
+// This is a small demonstration program showing how the Card and Deck classes are used.
+#include <iostream>    // Provides cout and cin
+#include <cstdlib>     // Provides EXIT_SUCCESS
+#include "card.h"
+#include "player.h"
+#include "deck.h"
+
+using namespace std;
+
+
+// PROTOTYPES for functions used by this demonstration program:
+
+
+Deck::Deck() {
+	//goes through and should instantiate eveything
+}
+
+void Deck::shuffle() {
+	
+}
+
+Card Deck::dealCard() {
+
+}
+
+int Deck::size() const{
+
+}
+
