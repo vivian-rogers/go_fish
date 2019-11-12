7650ae0bb4bfe6e7bfaf397f8b26a3a6ee3a55f6
cpoon <clarkap26@gmail.com>
2019-11-11 19:13:27 -0600

    minor changes to driver file

--- gone_fishing.cpp
+++ gone_fishing.cpp
@@ -47,27 +47,27 @@ int main( )
     	cout << p3.getName() <<" has : " << p3.showHand() << endl;
 	
 
-	while( (p1.getHandSize != 0 )&&(p2.getHandSize != 0 )&&(p3.getHandSize != 0 )){ //while there are cards still 
+	while( (p1.getHandSize != 0 )||(p2.getHandSize != 0 )||(p3.getHandSize != 0 )){ //while there are cards still 
 	       bool turnIsOngoing = true;
 	       int playernum = 1;
 	       while(turnIsOngoing) { 
 		        Player* player = nthPlayer(playernum); //assigns the base player from its number
-		       	if(player->getHandSize() == 0 ){ //if the player's hand is empty, draw a card
-				player->addCard(deck.dealCard());
-			}	
 		       	if(player->getHandSize() != 0 ){ //if it's still empty, don't do any of the playing stuff and get yo turn skipped
 				Card chosenCard = player->chooseCardFromHand(); 
 				Player* chosenPlayer = nthPlayer(exclusiveRand(player)); //picks an enemy player
 				cout << player->getName() << " is asking " << chosenPlayer->getName() << "for matches with "<< chosenCard <<;
 				turnIsOnGoing = false; //assumes turn is over, might be changed if a match is found
 				if(chosenPlayer->sameRankInHand(chosenCard)) {
-					//return cark with the same rank 
+					//return card with the same rank 
 					
 					//chosenPlayer->removeCardFromHand(cardWithSameRank);
 					//chosenPlayer->
-
-
 			}
+		       	if(player->getHandSize() == 0 ){ //if the player's hand is empty, draw a card
+				player->addCard(deck.dealCard());
+			}	
+
+
 
 
     cout << "Constructing Players\n";
